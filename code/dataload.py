changes to data preparation, and to refinenet - full resolution output + fixing loss function + fixing data augmentation (normalisation) so now works on test set

code/dataload.py
@@ -15,30 +15,23 @@ class ConfocalData(data.Dataset):
     """
 
 
-    def __init__(self, root, train=True, xtransform=None, ytransform=None):
+
+    def __init__(self, root, filename, xtransform=None, ytransform=None):
         
         self.root = os.path.expanduser(root)
-        self.train = train  # training set or test set
+        self.filename = filename
         self.xtransform=xtransform
         self.ytransform=ytransform
-       
+
         self.loadData()
 
 
     def loadData(self):
 
-        data = np.load(join(self.root, 'xData.npy'), mmap_mode='r') 
-        labels = np.load(join(self.root, 'yData.npy'), mmap_mode='r') 
+        self.data = np.load(join(self.root, 'x'+self.filename+'.npy'), mmap_mode='r') 
+        self.labels = np.load(join(self.root, 'y'+self.filename+'.npy'), mmap_mode='r') 
+        print 'Data and label shape: ', np.shape(self.data), np.shape(self.labels)
         
-        if self.train: 
-            self.data = data[:-128]
-            self.labels = labels[:-128]    
-            print 'Training data and labels: ', np.shape(self.data), np.shape(self.labels)
-        else: 
-            self.data = data[-128:]
-            self.labels = labels[-128:]
-            print 'Testing data and labels: ', np.shape(self.data), np.shape(self.labels)
-
 
     def __getitem__(self, index):
         """

code/dataprep.py
@@ -1,79 +1,89 @@
 from PIL import Image
 import numpy as np
 import os
+import argparse
 
-def upID(i, nx,ny):
-	if i == 0:
-		nx += 1
-		n = nx
-	elif i == 1:
-		ny += 1
-		n = ny
-	return n, nx, ny
+def get_args():
+	parser = argparse.ArgumentParser()
+	parser.add_argument('--imSize', default=256, type=int)
+
+	return parser.parse_args()
 
 
-directories = os.listdir('TrainingData/')
-fname = ['data.tiff', 'label.tiff']
-
-nx = 0
-ny = 0
-allImg = []
-allY = []
-
-for d in directories: 
-	if os.path.isdir(os.path.join('TrainingData', d)):
-
-		for i, f in enumerate(fname):
-			dirname, _ = os.path.splitext(os.path.join('TrainingData', d, f))
-			if not os.path.exists(dirname):
-				os.mkdir(dirname)
+def chopImage(fname, crops, label, train):
+	img = Image.open(fname)
+	img = np.array(img)
+	img = img[:,:,1] # green channel
 		
-			img = Image.open(os.path.join('TrainingData', d, f))
-			img = np.array(img)
-			img = img[:,:,1] # green channel
+	splitrows = np.split(img, crops, 0) # split into 16 rows (height = 1024/16 = 64 pixels)
 		
-			splitrows = np.split(img, 4, 0) # split into 16 rows (height = 1024/16 = 64 pixels)
-		
-			for eachrow in splitrows: 
-				splitcols = np.split(eachrow, 4, 1) # split each row into 16 (width = 1024/16 = 64 pixels)
+	allImg = []
+	for eachrow in splitrows: 
+		splitcols = np.split(eachrow, crops, 1) # split each row into 16 (width = 1024/16 = 64 pixels)
 			
-				for subimg in splitcols:
-					subimg = subimg[:,:, np.newaxis]
+		for subimg in splitcols:
+			subimg = subimg[:,:, np.newaxis]
 
-					if i == 0:
-						allImg.append(subimg)
-						n1,nx,ny = upID(i,nx,ny)
+			if label:
+				subimg[subimg>100] = 255
+				subimg[subimg<101] = 0
 
-						allImg.append(np.rot90(subimg,1))
-						n2,nx,ny = upID(i,nx,ny)
-						
-						allImg.append(np.rot90(subimg,2))
-						n3,nx,ny = upID(i,nx,ny)
-						
-						allImg.append(np.rot90(subimg,3))
-						n4,nx,ny = upID(i,nx,ny)
-					elif i == 1:
-						subimg[subimg>100] = 255
-						subimg[subimg<101] = 0
-						
-						allY.append(subimg)
-						n1,nx,ny = upID(i,nx,ny)
+			if train:
+				allImg.append(subimg)
+				allImg.append(np.rot90(subimg,1))
+				allImg.append(np.rot90(subimg,2))
+				allImg.append(np.rot90(subimg,3))
+				
+				allImg.append(np.fliplr(subimg))
+				allImg.append(np.fliplr(np.rot90(subimg,1)))
+				allImg.append(np.fliplr(np.rot90(subimg,2)))
+				allImg.append(np.fliplr(np.rot90(subimg,3)))
+			else:
+				allImg.append(subimg)
 
-						allY.append(np.rot90(subimg,1))
-						n2,nx,ny = upID(i,nx,ny)
-						
-						allY.append(np.rot90(subimg,2))
-						n3,nx,ny = upID(i,nx,ny)
-						
-						allY.append(np.rot90(subimg,3))
-						n4,nx,ny = upID(i,nx,ny)
-					
+	return np.array(allImg)
 
-					Image.fromarray(subimg[:,:,0]).save(os.path.join(dirname, str(n1) + '.tiff'))
-					Image.fromarray(np.rot90(subimg[:,:,0], 1)).save(os.path.join(dirname, str(n2) + '_r90.tiff'))
-					Image.fromarray(np.rot90(subimg[:,:,0], 2)).save(os.path.join(dirname, str(n3) + '_r180.tiff'))
-					Image.fromarray(np.rot90(subimg[:,:,0], 3)).save(os.path.join(dirname, str(n4) + '_r270.tiff'))		
 
-np.save('/home/ym1008/Documents/Tricellulin/SemSeg/data/xData.npy', np.array(allImg))
-np.save('/home/ym1008/Documents/Tricellulin/SemSeg/data/yData.npy', np.array(allY))
+if __name__=='__main__':
+	
+	opts = get_args()
 
+	directories = os.listdir('TrainingData/')
+	crops = 1024/opts.imSize
+	
+	allX = []
+	allY = []
+	for i,d in enumerate(directories): 
+
+		folder = os.path.join('TrainingData',d)
+		
+		if os.path.isdir(folder):
+
+			xfname = os.path.join(folder,'data.tiff')
+			yfname = os.path.join(folder,'label.tiff')
+
+			if i < 8:
+				X = chopImage(xfname, crops, False, True)
+				Y = chopImage(yfname, crops, True, True)
+			else:
+				X = chopImage(xfname, crops, False, False)
+				Y = chopImage(yfname, crops, True, False)
+							
+			allX.append(X)
+			allY.append(Y)
+
+			np.save(os.path.join(folder, 'x_'+str(opts.imSize)+'.npy'), X)
+			np.save(os.path.join(folder, 'y_'+str(opts.imSize)+'.npy'), Y)
+
+
+	# Train images 
+	np.save('/home/ym1008/Documents/Tricellulin/SemSeg/data/xTrain_' + str(opts.imSize)+'.npy', np.concatenate(allX[:8],0))
+	np.save('/home/ym1008/Documents/Tricellulin/SemSeg/data/yTrain_' + str(opts.imSize)+'.npy', np.concatenate(allY[:8],0))
+
+	# Test images 
+	np.save('/home/ym1008/Documents/Tricellulin/SemSeg/data/xTest_' + str(opts.imSize)+'.npy', np.concatenate(allX[8:10],0))
+	np.save('/home/ym1008/Documents/Tricellulin/SemSeg/data/yTest_' + str(opts.imSize)+'.npy', np.concatenate(allY[8:10],0))
+
+	# Validation images 
+	np.save('/home/ym1008/Documents/Tricellulin/SemSeg/data/xVal_' + str(opts.imSize)+'.npy', allX[10])
+	np.save('/home/ym1008/Documents/Tricellulin/SemSeg/data/yVal_' + str(opts.imSize)+'.npy', allY[10])

code/train.py
@@ -33,7 +33,8 @@ def get_args():
 	parser.add_argument('--batchSize', default=4, type=int)
 	parser.add_argument('--maxEpochs', default=150, type=int)
 	parser.add_argument('--lr', default=1e-5, type=float)
-	parser.add_argument('--imSize', default=224, type=int)  # divisible by 32.
+	parser.add_argument('--smooth', default=1.0, type=float)
+	parser.add_argument('--imSize', default=64, type=int)  # 64, 128, or 256.
 	parser.add_argument('--feats', default=256, type=int)  #multiple of filters to use
 	parser.add_argument('--commit', required=True, type=str)
 	parser.add_argument('--gpuNo', default=0, type=int)
@@ -46,6 +47,7 @@ def train(net, trainLoader, testLoader, opts):
 
 	####### Define optimizer #######
 	opt = optim.Adam(net.parameters(), lr=opts.lr)
+	smooth = opts.smooth;
 
 	useCUDA = torch.cuda.is_available()
 
@@ -68,20 +70,13 @@ def train(net, trainLoader, testLoader, opts):
 		T = time()
 		for i, (x,y) in enumerate(trainLoader, 0):
 
-			bs, ncrops, c, h, w = x.size()
-			x = x.view(-1, c, h, w)
-
-			bs, ncrops, c, h, w = y.size()
-			y = y.view(-1, c, h, w)
-
 			x = Variable(x).cuda() if useCUDA else Variable(x)
 			y = y.cuda() if useCUDA else y
 
 			output = net(x)
-			output = interpolate(output, scale_factor=(4,4))
-		
+			
 			# calculate the loss. 
-			loss = 2 * torch.sum(output * y, (1,2,3)) / (torch.sum(output**2 + y**2, (1, 2, 3)))
+			loss = 1-2 * torch.sum(output * y, (1,2,3)) / (torch.sum(output**2 + y**2, (1, 2, 3)) + smooth)
 			loss = torch.mean(loss)
 
 			losses['train'].append(loss.item())	
@@ -95,7 +90,9 @@ def train(net, trainLoader, testLoader, opts):
 					% (e, i, loss.item(), time()-T)
 
 
-		save_image(output, join(exDir,'rec_train_e'+str(e)+'.png'), normalize=True, nrow = 5)
+		save_image(output, join(exDir,'rec_train_e'+str(e)+'.png'), normalize=True, scale_each=True, nrow = 5)
+		save_image(x, join(exDir,'xtrain_e'+str(e)+'.png'), normalize=True, scale_each=True, nrow = 5)
+		save_image(y, join(exDir,'ytrain_e'+str(e)+'.png'), normalize=True, scale_each=True, nrow = 5)
 		
 		# Test
 		net.eval()
@@ -108,10 +105,9 @@ def train(net, trainLoader, testLoader, opts):
 			y = y.cuda() if useCUDA else y
 
 			output = net(x)
-			output = interpolate(output, scale_factor=(4,4))
-		
+			
 			# calculate the loss. 
-			loss = 2 * torch.sum(output * y, (1,2,3)) / (torch.sum(output**2 + y**2, (1, 2, 3)))
+			loss = 1-2 * torch.sum(output * y, (1,2,3)) / (torch.sum(output**2 + y**2, (1, 2, 3)))
 			testloss = testloss + torch.sum(loss).item()
 			n = n + loss.shape[0]
 
@@ -121,9 +117,10 @@ def train(net, trainLoader, testLoader, opts):
 		plot_losses(losses, exDir, epochs=e+1)
 
 		# save the segmentations of the test images... train images too? Few examples? 
-		save_image(output, join(exDir,'rec_test_e'+str(e)+'.png'), normalize=True, nrow = opts.batchSize/2)
+		save_image(output, join(exDir,'rec_test_e'+str(e)+'.png'), normalize=True, scale_each=True, nrow = opts.batchSize/2)
 		if e == 0:
-			save_image(y, join(exDir,'orig_test.png'), normalize=True, nrow = opts.batchSize/2)
+			save_image(y, join(exDir,'orig_test.png'), normalize=True, scale_each=True, nrow = opts.batchSize/2)
+			save_image(x, join(exDir,'orig_xtest.png'), normalize=True, scale_each=True, nrow = opts.batchSize/2)
 	
 
 		####### Save params #######
@@ -140,22 +137,18 @@ if __name__=='__main__':
 
 	xtrainTransform = transforms.Compose([transforms.ToPILImage(), transforms.Grayscale(3),\
 		transforms.ColorJitter(brightness=0.5, contrast=0.5), transforms.ToTensor(),\
-		transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)), transforms.ToPILImage(),\
-		transforms.FiveCrop(opts.imSize), transforms.Lambda(lambda crops: torch.stack([transforms.ToTensor()(crop) for crop in crops]))])
-
-	ytrainTransform = transforms.Compose([transforms.ToPILImage(), transforms.FiveCrop(opts.imSize),\
-	 	transforms.Lambda(lambda crops: torch.stack([transforms.ToTensor()(crop) for crop in crops]))])
-
-	trainDataset = ConfocalData(root=opts.root, train=True, xtransform=xtrainTransform, ytransform = ytrainTransform)
-	trainLoader = torch.utils.data.DataLoader(trainDataset, batch_size=opts.batchSize, shuffle=True)
-
-
+		transforms.Normalize((0.5,0.5,0.5),(0.5,0.5,0.5))])
+	
 	xtestTransform = transforms.Compose([transforms.ToPILImage(), transforms.Grayscale(3),\
 		transforms.ToTensor(), transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
 
-	ytestTransform = transforms.Compose([transforms.ToPILImage(), transforms.ToTensor()])
+	yTransform = transforms.Compose([transforms.ToPILImage(), transforms.ToTensor()])
+
+
+	trainDataset = ConfocalData(root=opts.root, filename='Train_'+str(opts.imSize), xtransform=xtrainTransform, ytransform = yTransform)
+	trainLoader = torch.utils.data.DataLoader(trainDataset, batch_size=opts.batchSize, shuffle=True)
 	
-	testDataset = ConfocalData(root=opts.root, train=False, xtransform=xtestTransform, ytransform=ytestTransform)
+	testDataset = ConfocalData(root=opts.root, filename='Val_'+str(opts.imSize), xtransform=xtestTransform, ytransform=yTransform)
 	testLoader = torch.utils.data.DataLoader(testDataset, batch_size=opts.batchSize, shuffle=False)
 
 	print 'Data loaders ready.'

refinenet/refinenet_4cascade.py
@@ -54,6 +54,8 @@ class BaseRefineNet4Cascade(nn.Module):
                 for param in layer.parameters():
                     param.requires_grad = False
 
+        self.layer0_rn = nn.Conv2d(
+            3, features, kernel_size=3, stride=1, padding=1, bias=False)
         self.layer1_rn = nn.Conv2d(
             256, features, kernel_size=3, stride=1, padding=1, bias=False)
         self.layer2_rn = nn.Conv2d(
@@ -73,6 +75,8 @@ class BaseRefineNet4Cascade(nn.Module):
                                          (features, input_size // 8))
         self.refinenet1 = RefineNetBlock(features, (features, input_size // 8),
                                          (features, input_size // 4))
+        self.refinenet0 = RefineNetBlock(features, (features, input_size // 4),
+                                         (features, input_size))
 
         self.output_conv = nn.Sequential(
             ResidualConvUnit(features), ResidualConvUnit(features),
@@ -82,7 +86,8 @@ class BaseRefineNet4Cascade(nn.Module):
                 kernel_size=1,
                 stride=1,
                 padding=0,
-                bias=True))
+                bias=True), 
+            nn.Sigmoid())
 
     def forward(self, x):
 
@@ -91,6 +96,7 @@ class BaseRefineNet4Cascade(nn.Module):
         layer_3 = self.layer3(layer_2)
         layer_4 = self.layer4(layer_3)
 
+        layer_0_rn = self.layer0_rn(x)
         layer_1_rn = self.layer1_rn(layer_1)
         layer_2_rn = self.layer2_rn(layer_2)
         layer_3_rn = self.layer3_rn(layer_3)
@@ -100,7 +106,8 @@ class BaseRefineNet4Cascade(nn.Module):
         path_3 = self.refinenet3(path_4, layer_3_rn)
         path_2 = self.refinenet2(path_3, layer_2_rn)
         path_1 = self.refinenet1(path_2, layer_1_rn)
-        out = self.output_conv(path_1)
+        path_0 = self.refinenet0(path_1, layer_0_rn)
+        out = self.output_conv(path_0)
 
         return out
 